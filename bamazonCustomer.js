c316c1bd76ae0391b05f4c131497894d6f2f6315
Jacob Thomas <jcthomas00@gmail.com>
2017-09-14 14:54:59 -0500

    add comments to js file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,17 +6,20 @@ var mysql = require('mysql'),
 		user: 'root',
 		password: 'madonna',
 		database: 'bamazon'
-	});
-require ('console.table');
+	});							//Get dependencies
+require ('console.table');		//Easy display of mySQL data in table format
 
+var Bamazon = function(){};		//Because objects are cool
 
-var Bamazon = function(){};
-
+/*************		Method to get data from mySQL and pass it to the callback		***************/
 Bamazon.prototype.queryProducts = function(sql = "select * from products", callback, options){
 	connection.connect((error)=> {
+			//execute the sql passed in
 			connection.query(sql, (error, data)=>{
 				if (error){console.log(error);} else{
+					//do the callback if it was passed in
 					if(typeof callback === "function"){
+						//send options to the callback if passed in
 						if (options){
 							callback(data, options);
 						} else{
@@ -29,16 +32,22 @@ Bamazon.prototype.queryProducts = function(sql = "select * from products", callb
 	});
 }
 
+/*************		Method to check the dataset for validity and place order 		***************/
 Bamazon.prototype.stockCheck = function(dataset, parameters) {
 	if(dataset.length > 0){
-		let query = `Update products set stock_quantity = ${dataset[0].stock_quantity-parameters.quantity} where item_id = ${parameters.id}`;
+		let query = `Update products set stock_quantity = ${dataset[0].stock_quantity - parameters.quantity} 
+					where item_id = ${parameters.id}`;
+		//subtract the number ordered from the product's quantity
 		parameters.self.queryProducts(query);
-		console.log(`Your order total is: $${parameters.quantity*dataset[0].price}`);
+		//print user total
+		console.log(`Your order total is: $${parameters.quantity * dataset[0].price}`);
 	}else{
+		//either user entered invalid id or we don't have enough stock
 		console.log("Insufficient quantity or invalid product. Please try again.");
 	}
 }
 
+/*************				Method to ask user to buy something				***************/
 Bamazon.prototype.buySomething = function() {
 	inquirer.prompt([
 		{
@@ -51,9 +60,11 @@ Bamazon.prototype.buySomething = function() {
 		}
 	]).then((res)=>{
 		let query = `select * from products where item_id = ${res.productID} AND stock_quantity > ${res.productQuantity-1}`;
+		//look for user entered product and try to place order
 		this.queryProducts(query, this.stockCheck, {'quantity':res.productQuantity, 'id':res.productID, 'self': this});
 	});
 };
 
+//create a new bamazon store object to destroy Bezos and beg users to order
 var bamazon = new Bamazon();
 bamazon.queryProducts(undefined , console.table);
